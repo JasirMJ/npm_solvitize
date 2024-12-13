d6147fdf646e4f79dc9836649e563142ffa53049
jasirmj <jasirmj@gmail.com>
2024-12-14 01:12:43 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-module.exports = function sayHello() {
+// module.exports = function sayHello() {
+//     console.log("Hello, world from Solvitize!");
+// };
+
+
+
+export const sayHello = () => {
     console.log("Hello, world from Solvitize!");
 };
-
 export const sayThanks = () => {
     console.log("Thanks for using Solvitize!");
 };
