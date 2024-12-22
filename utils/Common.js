df4e0c5d74cb0c59014a36bc0e1c47499380252d
jasirmj <jasirmj@gmail.com>
2024-12-22 06:44:44 +0530

    request formatter added

--- utils/Common.js
+++ utils/Common.js
@@ -33,3 +33,106 @@ export const getCountdownTime = (targetTime) => {
     return obj;
 
 };
+
+
+
+export const formatResponse = (response, isSuccess) => {
+    try {
+        let status_code = null;
+
+        console.log("Format response ", response)
+        console.log("Format response status ", response.status)
+
+        if (response === undefined) {
+            return {
+                status: false,
+                message: "Seems like server is down, Please try again later",
+                data: null,
+            }
+        }
+
+        status_code = response && response.status ? response.status : 200
+
+        // Django error response
+        // {"details":"some message"}
+        // if response has key detail 
+        if (response?.data?.detail) {
+
+            // TODO:  Need to handle invalid username or password, form valitation etc 
+
+            return {
+                status: false,
+                message: response?.data?.detail || "An error occurred",
+                data: response?.data || null,
+                status_code: status_code
+            };
+        }
+        else if (isSuccess) {
+
+            if (response?.data?.Data) {
+                // usually for POST request
+                return {
+                    status: response?.data?.Status,
+                    message: response?.data?.Message || "POST Request successful",
+                    data: response?.data.Data || null,
+                    status_code: status_code
+                };
+            } else {
+                // For getting list of data from API
+                return {
+                    status: true,
+                    message: response?.data?.message || "GET Request successful",
+                    data: response?.data || null,
+                    status_code: status_code
+                };
+            }
+
+
+        }
+        else {
+            if (status_code === 404) {
+                return {
+                    status: false,
+                    message: "Page not found",// for "+response?.config?.url || "Unknown url",
+                    data: response?.data || null,
+                    status_code: status_code
+                };
+            }
+            else if (status_code === 400) {
+                return {
+                    status: false,
+                    message: "Bad request, Make sure request is correct",// for "+response?.config?.url || "Unknown url",
+                    data: response?.data || null,
+                    status_code: status_code
+                };
+            } else if (status_code === 500) {
+                return {
+                    status: false,
+                    message: "Internal server error, Please contact support",
+                    data: response?.data || null,
+                    status_code: status_code
+                };
+            }
+            else {
+
+                return {
+                    status: false,
+                    message: response?.data?.message || response?.statusText || "An error occurred",
+                    data: response?.data || null,
+                    status_code: status_code
+                };
+            }
+        }
+    }
+    catch (error) {
+
+        console.log("Unexpected error occurred while processing your request", error)
+        return {
+            status: false,
+            message: "Unexpected error occurred while processing your request",
+            data: null,
+        }
+    }
+};
+
+
